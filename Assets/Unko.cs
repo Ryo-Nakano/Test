6e92b6f755135f9fe0d6fcafd66b1ac161b3f04d
Ryohei Nanano <matsuhara.9999ryo@gmail.com>
2018-06-29 20:49:20 +0900

    ▶︎わっしょい

---
+++ Assets/Unko.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unko : MonoBehaviour {
+
+	[SerializeField] float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+	}
+}
